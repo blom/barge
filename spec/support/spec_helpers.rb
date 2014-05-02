44ab12bbe2b2c8cda52afda7f3a0d36166b285cc
Ørjan Blom <blom@blom.tv>
2014-05-02 18:55:04 +0200

    Add fixture helper

--- spec/support/spec_helpers.rb
+++ spec/support/spec_helpers.rb
@@ -1,4 +1,8 @@
 module SpecHelpers
+  def fixture(name)
+    File.read(File.expand_path("../../fixtures/#{name}.json", __FILE__))
+  end
+
   def stub_request!(verb, path)
     stub_request(verb, File.join(Barge::Client::DIGITAL_OCEAN_URL, path))
       .with(headers: { authorization: 'some_token' })

1c8501c17b02c001dd7d21e7d8c713291fed3463
Ørjan Blom <blom@blom.tv>
2014-05-02 18:53:03 +0200

    Refine test

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -1,9 +1,11 @@
 shared_examples_for 'a resource' do
   specify { expect(described_class).to include Barge::Resource::Base }
 
-  describe '.new' do
-    it 'takes one argument and returns it from #faraday' do
-      expect(described_class.new(123).faraday).to be 123
+  describe '#faraday' do
+    let(:value) { '123' }
+
+    it 'returns the object passed to .new' do
+      expect(described_class.new(value).faraday).to be value
     end
   end
 end

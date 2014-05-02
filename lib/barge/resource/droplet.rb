70cc6c7b07437f3de9c2b569d269a0837d6b996f
Ørjan Blom <blom@blom.tv>
2014-05-02 18:56:43 +0200

    Add Droplet#show

--- lib/barge/resource/droplet.rb
+++ lib/barge/resource/droplet.rb
@@ -2,6 +2,10 @@ module Barge
   module Resource
     class Droplet
       include Resource::Base
+
+      def show(droplet_id)
+        request(:get, "/droplets/#{droplet_id}")
+      end
     end
   end
 end
--- spec/barge/resource/droplet_spec.rb
+++ spec/barge/resource/droplet_spec.rb
@@ -2,4 +2,15 @@ require 'spec_helper'
 
 describe Barge::Resource::Droplet do
   it_behaves_like 'a resource'
+
+  let(:barge) { Barge::Client.new(access_token: 'some_token') }
+  let(:droplet) { barge.droplet }
+
+  describe '#show' do
+    it 'returns information about a specific droplet' do
+      stub_request!(:get, '/droplets/10')
+        .to_return(body: fixture('droplets/show'), status: 200)
+      expect(droplet.show(10).droplet.name).to eq 'test222'
+    end
+  end
 end

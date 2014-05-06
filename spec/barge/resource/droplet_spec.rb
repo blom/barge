80a4c8b7c75aab5645a96e4f99817f72ed60fcf8
Ørjan Blom <blom@blom.tv>
2014-05-06 07:13:18 +0200

    Update Droplet#show fixture and expectation

--- spec/barge/resource/droplet_spec.rb
+++ spec/barge/resource/droplet_spec.rb
@@ -8,7 +8,7 @@ describe Barge::Resource::Droplet do
     it 'returns information about a specific droplet' do
       stubbed_request = stub_request!(:get, '/droplets/10')
         .to_return(body: fixture('droplets/show'), status: 200)
-      expect(droplet.show(10).droplet.name).to eq 'test222'
+      expect(droplet.show(10).snapshots.first.id).to be 32
       expect(stubbed_request).to have_been_requested
     end
   end

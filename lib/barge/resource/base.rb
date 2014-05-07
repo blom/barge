ae6695f7456ed6189251a03086c246400dcc7ce4
Ørjan Blom <blom@blom.tv>
2014-05-07 20:23:25 +0200

    Process arrays too

--- lib/barge/resource/base.rb
+++ lib/barge/resource/base.rb
@@ -10,7 +10,16 @@ module Barge
       private
 
       def request(verb, *args)
-        Hashie::Mash.new faraday.public_send(verb, *args).body
+        response = faraday.public_send(verb, *args)
+        process_response(response)
+      end
+
+      def process_response(response)
+        if response.body.kind_of?(Array)
+          response.body.map { |entry| Hashie::Mash.new(entry) }
+        else
+          Hashie::Mash.new(response.body)
+        end
       end
     end
   end

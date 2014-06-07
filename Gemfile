dfc7425a6f7c773b7b9bceb718cd2d28dca15f09
Ørjan Blom <blom@blom.tv>
2014-06-07 08:44:18 +0200

    Set up Coveralls

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 gemspec
 
+gem 'coveralls', require: false
 gem 'rake'
 gem 'rspec'
 gem 'rubocop'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,16 @@
 if ENV['SIMPLECOV']
   require 'simplecov'
   SimpleCov.start { add_filter '/spec/' }
+elsif ENV['TRAVIS']
+  require 'coveralls'
+  Coveralls.wear! { add_filter '/spec/' }
 end
 
 require_relative '../lib/barge'
 Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
 
 require 'webmock/rspec'
+WebMock.disable_net_connect!(allow: 'coveralls.io')
 
 RSpec.configure do |config|
   config.include SpecHelpers

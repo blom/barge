944f0ec6125d9adcb24ee282084a10e8951af997
Alexey Mirniy <alexey.mirniy@gmail.com>
2014-11-03 12:52:13 +0200

    new: specs for Client#timeouts parameter

--- spec/barge/client_spec.rb
+++ spec/barge/client_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
 describe Barge::Client do
-  describe '#access_token' do
-    let(:token) { 'some_token' }
+  let(:token) { 'some_token' }
+  let(:timeouts) { 100 }
+  let(:default_timeouts) {described_class.const_get :TIMEOUTS}
 
+  describe '#access_token' do
     it 'can be set using a hash' do
       barge = described_class.new(access_token: token)
       expect(barge.access_token).to eq(token)
@@ -19,6 +21,25 @@ describe Barge::Client do
         expect { described_class.new }
           .to raise_error(ArgumentError, 'missing access_token')
       end
+    end
+  end
+
+  describe '#timeouts' do
+    it 'is optional, client should use default TIMEOUTS' do
+      barge = described_class.new(access_token: token)
+      expect(barge.timeouts).to eq(default_timeouts)
+    end
+    it 'can be set using a hash' do
+      barge = described_class.new(access_token: token, timeouts: timeouts)
+      expect(barge.timeouts).to eq(timeouts)
+    end
+
+    it 'can be set using a block' do
+      barge = described_class.new { |c| 
+        c.access_token = token
+        c.timeouts = timeouts
+      }
+      expect(barge.timeouts).to eq(timeouts)
     end
   end
 end
